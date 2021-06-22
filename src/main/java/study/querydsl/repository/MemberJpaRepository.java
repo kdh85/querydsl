2eef572034ace83cfac63bc40d95c4b7c49b3b01
LAPTOP-A8710DGI\kdh85 <modudamba@naver.com>
2021-06-22 13:06:14 +0900

    feat: querdsl 동적 쿼리 생성 샘플

--- src/main/java/study/querydsl/repository/MemberJpaRepository.java
+++ src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -1,7 +1,13 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
 import study.querydsl.entity.Member;
 
 import javax.persistence.EntityManager;
@@ -9,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
 
 @Repository
 public class MemberJpaRepository {
@@ -59,4 +66,81 @@ public class MemberJpaRepository {
                 .fetch();
     }
 
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if(StringUtils.hasText(condition.getUsername())){
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+
+        if(StringUtils.hasText(condition.getTeamName())){
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+
+        if(condition.getAgeGoe() != null){
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+
+        if(condition.getAgeLoe() != null){
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        //쿼리 실행할때 조심해야 할 점
+        //where 조건이 전부 null일 경우 전체 데이터 풀조회하기 때문에
+        //데이터 건이 많아진 경우 부하가 엄청 발생함. 가급적 페이징 쿼리 식으로 수정하는 것을 권장.
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition){
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        //재활용이 가능한 컨디션 조건들 생성.
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    //condition을 조립해서 사용하는 경우.
+    private BooleanExpression ageBetween(int ageLoe, int ageGoe){
+        return ageGoe(ageLoe).and(ageGoe(ageGoe));
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return StringUtils.hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
 }
--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
-
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +43,36 @@ class MemberJpaRepositoryTest {
 
         List<Member> member1_qMembers = memberJpaRepository.findByUsername_QueryDsl("member1");
         assertThat(member1_qMembers).containsExactly(member);
+    }
+
+    @Test
+    void searchTest() {
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        entityManager.persist(teamA);
+        entityManager.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        entityManager.persist(member1);
+        entityManager.persist(member2);
+        entityManager.persist(member3);
+        entityManager.persist(member4);
+
+        MemberSearchCondition condition = new MemberSearchCondition();
+        condition.setAgeGoe(35);
+        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> memberTeamDtos = memberJpaRepository.searchByBuilder(condition);
+
+        assertThat(memberTeamDtos).extracting("username").containsExactly("member4");
+
+        List<MemberTeamDto> search = memberJpaRepository.search(condition);
+
+        assertThat(search).extracting("username").containsExactly("member4");
     }
 }
